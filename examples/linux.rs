4905c13ae1e77cad1a325d66000de930c1e4abbb
Diego Barrios Romero <eldruin@gmail.com>
2018-11-07 20:27:34 +0100

    Add example

---
+++ examples/linux.rs
@@ -0,0 +1,16 @@
+extern crate embedded_hal;
+use embedded_hal::adc::OneShot;
+extern crate linux_embedded_hal;
+#[macro_use(block)]
+extern crate nb;
+extern crate ads1x1x;
+
+use linux_embedded_hal::I2cdev;
+use ads1x1x::{ Ads1x1x, SlaveAddr, channel };
+
+fn main() {
+    let dev = I2cdev::new("/dev/i2c-1").unwrap();
+    let mut adc = Ads1x1x::new_ads1013(dev, SlaveAddr::default());
+    let measurement = block!(adc.read(&mut channel::A0)).unwrap();
+    println!("Measurement: {}", measurement);
+}
