b9efeb8b88fbd223e5fa948b50e4b9ba78c54540
Diego Barrios Romero <eldruin@gmail.com>
2018-11-08 18:57:50 +0100

    Create macro for IC markers

--- src/devices/ic.rs
+++ src/devices/ic.rs
@@ -9,14 +9,15 @@ pub trait Resolution : super::private::Sealed {
     const BITS : ResolutionBits;
 }
 
-/// ADS1013 IC marker
-pub struct Ads1013(());
-impl Resolution for Ads1013 {
-    const BITS: ResolutionBits = ResolutionBits::_12;
+macro_rules! ic_marker {
+    ($name:ident, $resolution:ident) => {
+        /// IC marker
+        pub struct $name(());
+        impl Resolution for $name {
+            const BITS: ResolutionBits = ResolutionBits::$resolution;
+        }
+    };
 }
 
-/// ADS1113 IC marker
-pub struct Ads1113(());
-impl Resolution for Ads1113 {
-    const BITS: ResolutionBits = ResolutionBits::_16;
-}
+ic_marker!(Ads1013, _12);
+ic_marker!(Ads1113, _16);
