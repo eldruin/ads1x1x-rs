8bc86b228f245e1056a79a7f7d6d97641f1bb745
Diego Barrios Romero <eldruin@gmail.com>
2018-11-08 18:59:02 +0100

    Add unit tests for measurement conversion

--- src/devices/ads1x1x/mode/mod.rs
+++ src/devices/ads1x1x/mode/mod.rs
@@ -24,3 +24,24 @@ where
 
 mod oneshot;
 mod continuous;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn convert_12_bits() {
+        assert_eq!(    0, convert_measurement::<ic::Ads1013>(0));
+        assert_eq!( 2047, convert_measurement::<ic::Ads1013>(0x7FFF));
+        assert_eq!(-2048, convert_measurement::<ic::Ads1013>(0x8000));
+        assert_eq!(   -1, convert_measurement::<ic::Ads1013>(0xFFFF));
+    }
+
+    #[test]
+    fn convert_16_bits() {
+        assert_eq!(     0, convert_measurement::<ic::Ads1113>(0));
+        assert_eq!( 32767, convert_measurement::<ic::Ads1113>(0x7FFF));
+        assert_eq!(-32768, convert_measurement::<ic::Ads1113>(0x8000));
+        assert_eq!(    -1, convert_measurement::<ic::Ads1113>(0xFFFF));
+    }
+}
