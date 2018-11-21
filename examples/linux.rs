c68343af9e680323c74726e9f071ddcbe236b78a
Diego Barrios Romero <eldruin@gmail.com>
2018-11-21 09:54:14 +0100

    Rename variable

--- examples/linux.rs
+++ examples/linux.rs
@@ -12,8 +12,8 @@ fn main() {
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
     let address = SlaveAddr::default();
     let mut adc = Ads1x1x::new_ads1013(dev, address);
-    let measurement = block!(adc.read(&mut channel::DifferentialA0A1)).unwrap();
-    println!("Measurement: {}", measurement);
+    let value = block!(adc.read(&mut channel::DifferentialA0A1)).unwrap();
+    println!("Measurement: {}", value);
     // get I2C device back
     let _dev = adc.destroy_ads1013();
 }
