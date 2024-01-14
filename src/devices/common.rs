8ceb10dc2983eddcc9c7467f1c1761521fd98251
Markus Reiter <me@reitermark.us>
2024-01-14 06:49:15 +0100

    Remove `reset_internal_driver_state`.

--- src/devices/common.rs
+++ src/devices/common.rs
@@ -37,19 +37,4 @@ where
         };
         Ok(!config.is_high(BitFlags::OS))
     }
-
-    /// Reset the internal state of this driver to the default values.
-    ///
-    /// *Note:* This does not alter the state or configuration of the device.
-    ///
-    /// This resets the cached configuration register value in this driver to
-    /// the power-up (reset) configuration of the device.
-    ///
-    /// This needs to be called after performing a reset on the device, for
-    /// example through an I2C general-call Reset command, which was not done
-    /// through this driver to ensure that the configurations in the device
-    /// and in the driver match.
-    pub fn reset_internal_driver_state(&mut self) {
-        self.config = Config::default();
-    }
 }
