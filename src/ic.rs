923effbde3641d449b213e87f3a35bda00481f43
Markus Reiter <me@reitermark.us>
2024-01-19 17:58:55 +0100

    Make `convert_threshold` panic.

--- src/ic.rs
+++ src/ic.rs
@@ -1,13 +1,12 @@
-use crate::Error;
-
 pub(crate) struct Resolution12Bit;
 
 impl Resolution12Bit {
-    pub fn convert_threshold<E>(value: i16) -> Result<u16, Error<E>> {
+    pub fn convert_threshold(value: i16) -> u16 {
         if !(-2048..=2047).contains(&value) {
-            return Err(Error::InvalidInputData);
+            panic!("Threshold must be between -2048 and 2047, but is {value}.")
         }
-        Ok((value << 4) as u16)
+
+        (value << 4) as u16
     }
 
     pub fn convert_measurement(register_data: u16) -> i16 {
@@ -25,8 +24,8 @@ impl Resolution12Bit {
 pub(crate) struct Resolution16Bit;
 
 impl Resolution16Bit {
-    pub fn convert_threshold<E>(value: i16) -> Result<u16, Error<E>> {
-        Ok(value as u16)
+    pub fn convert_threshold(value: i16) -> u16 {
+        value as u16
     }
 
     pub fn convert_measurement(register_data: u16) -> i16 {
@@ -39,7 +38,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn convert_measurement_12_bits() {
+    fn convert_measurement_12bit() {
         assert_eq!(0, Resolution12Bit::convert_measurement(0));
         assert_eq!(2047, Resolution12Bit::convert_measurement(0x7FFF));
         assert_eq!(-2048, Resolution12Bit::convert_measurement(0x8000));
@@ -47,44 +46,36 @@ mod tests {
     }
 
     #[test]
-    fn convert_measurement_16_bits() {
+    fn convert_measurement_16bit() {
         assert_eq!(0, Resolution16Bit::convert_measurement(0));
         assert_eq!(32767, Resolution16Bit::convert_measurement(0x7FFF));
         assert_eq!(-32768, Resolution16Bit::convert_measurement(0x8000));
         assert_eq!(-1, Resolution16Bit::convert_measurement(0xFFFF));
     }
 
-    fn assert_invalid_input_data<E>(result: Result<u16, Error<E>>) {
-        match result {
-            Err(Error::InvalidInputData) => (),
-            _ => panic!("InvalidInputData error was not returned."),
-        }
-    }
-
     #[test]
-    fn check_assert_matches() {
-        assert_invalid_input_data::<()>(Err(Error::InvalidInputData));
+    fn convert_threshold_12bit() {
+        assert_eq!(0, Resolution12Bit::convert_threshold(0));
+        assert_eq!(0x7FF0, Resolution12Bit::convert_threshold(2047));
+        assert_eq!(0x8000, Resolution12Bit::convert_threshold(-2048));
+        assert_eq!(0xFFF0, Resolution12Bit::convert_threshold(-1));
     }
 
     #[test]
     #[should_panic]
-    fn check_assert_fails() {
-        assert_invalid_input_data::<()>(Ok(0));
+    fn convert_threshold_12bit_invalid_low() {
+        Resolution12Bit::convert_threshold(-2049);
     }
 
     #[test]
-    fn convert_threshold_12_bits() {
-        assert_invalid_input_data::<()>(Resolution12Bit::convert_threshold::<()>(2048));
-        assert_invalid_input_data::<()>(Resolution12Bit::convert_threshold::<()>(-2049));
-        assert_eq!(Ok(0), Resolution12Bit::convert_threshold::<()>(0));
-        assert_eq!(Ok(0x7FF0), Resolution12Bit::convert_threshold::<()>(2047));
-        assert_eq!(Ok(0x8000), Resolution12Bit::convert_threshold::<()>(-2048));
-        assert_eq!(Ok(0xFFF0), Resolution12Bit::convert_threshold::<()>(-1));
+    #[should_panic]
+    fn convert_threshold_12bit_invalid_high() {
+        Resolution12Bit::convert_threshold(2048);
     }
 
     #[test]
-    fn convert_threshold_16_bits() {
-        assert_eq!(Ok(0x7FFF), Resolution16Bit::convert_threshold::<()>(32767));
-        assert_eq!(Ok(0x8000), Resolution16Bit::convert_threshold::<()>(-32768));
+    fn convert_threshold_16bit() {
+        assert_eq!(0x7FFF, Resolution16Bit::convert_threshold(32767));
+        assert_eq!(0x8000, Resolution16Bit::convert_threshold(-32768));
     }
 }
